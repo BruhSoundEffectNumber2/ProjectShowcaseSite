161cd94aa7e16fb9d786c2cdd4e870e8011697c3
Damon <crazygamerdl@hotmail.com>
2022-04-05 10:02:22 -0700

    started on solar system model

--- tableContents.js
+++ tableContents.js
@@ -10,11 +10,5 @@ for (const element of document.getElementById("tableContentsBtns").children) {
 function tocButtonClicked(event) {
     // Get the id we are going to from the event
     const id = event.target.id.split("_")[1];
-    
-    // Have a 5% chance to go to bigweld instead of scrolling
-    if (Math.random() >= 0.05) {
-        window.scroll({ top: document.getElementById(id).offsetTop, behavior: "smooth" });
-    } else {
-        window.open("bigweld.html", '_blank');
-    }
+    window.scroll({ top: document.getElementById(id).offsetTop, behavior: "smooth" });
 }
